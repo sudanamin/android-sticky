cceac587dbd78c5588492417af5b2e44c169705e
sudanamin <sudanamin@hotmail.com>
2018-02-03 20:59:21 +0400

    add dependency and basic firebase intstanse and reference

--- stickykeep/app/src/main/java/com/example/ammostafa/stickykeep/MainActivity.java
+++ stickykeep/app/src/main/java/com/example/ammostafa/stickykeep/MainActivity.java
@@ -5,11 +5,16 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseFirestore mFirestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +31,11 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // Firestore
+        mFirestore = FirebaseFirestore.getInstance();
+        DocumentReference restRef = mFirestore.collection("restaurants").document();
+
     }
 
     @Override
