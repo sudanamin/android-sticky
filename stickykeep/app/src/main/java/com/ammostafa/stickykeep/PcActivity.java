088dbaa04ede20994bc260ec277b3d984e8ed6ac
sudanamin <sudanamin@hotmail.com>
2018-03-23 16:25:34 +0400

    unkown 1.4

--- stickykeep/app/src/main/java/com/ammostafa/stickykeep/PcActivity.java
+++ stickykeep/app/src/main/java/com/ammostafa/stickykeep/PcActivity.java
@@ -2,13 +2,17 @@ package com.ammostafa.stickykeep;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
 
 public class PcActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+
         setContentView(R.layout.activity_pc);
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
--- stickykeep/app/src/main/java/com/ammostafa/stickykeep/WebActivity.java
+++ stickykeep/app/src/main/java/com/ammostafa/stickykeep/WebActivity.java
@@ -2,12 +2,16 @@ package com.ammostafa.stickykeep;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
 
 public class WebActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+
         setContentView(R.layout.activity_web);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
